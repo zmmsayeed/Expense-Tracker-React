773ce4a4e592b333df3d6b53fad3818012b18807
Zimam Sayeed ~Ubuntu <zmmsayeed@gmail.com>
2021-01-23 07:48:23 +0530

    Initial   Front End setup completed

---
+++ expense-tracker/src/App.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import MainScreen from './screens/MainScreen'
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <MainScreen />
+    )
+  }
+  
+}
+
+
+export default App;
---
+++ expense-tracker/src/screens/MainScreen/index.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { Props, State } from './interfaces'
+
+class MainScreen extends React.Component<Props, State> {
+    // constructor(props: Props) {
+    //     super(props);
+    // }
+
+    render() {
+        return(
+            <p>Main Screen</p>
+        )
+    }
+}
+
+export default MainScreen;
