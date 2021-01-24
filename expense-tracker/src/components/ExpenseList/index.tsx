e822d884cbfbe6b0d12f899eb34726f934eb107f
Zimam Sayeed ~Ubuntu <zmmsayeed@gmail.com>
2021-01-24 18:07:20 +0530

    Added warning when user clicks on delete expense

--- expense-tracker/src/components/ExpenseList/index.tsx
+++ expense-tracker/src/components/ExpenseList/index.tsx
@@ -3,45 +3,59 @@ import moment from 'moment';
 import { State, Props } from './interface';
 
 // importing antd components
-import { List } from 'antd'
+import { List, Popconfirm } from 'antd'
 
 class ExpenseList extends React.Component<Props, State> {
 
-    render() {
+  confirmDelete = () => {
+    console.log("Deleting")
+  }
 
-      let { expenseList } = this.props;
+  render() {
 
-      if(expenseList.length) {
-        return(
-          <List
-            itemLayout="horizontal"
-            dataSource={expenseList}
-            renderItem={item => (
-              <List.Item
-                actions={['Edit', 'Delete']}
-              >
-                  <List.Item.Meta
-                    title={
-                      <div className="row">
-                          <div className="col-md-6">{item.amount}</div>
-                          <div className="col-md-6 text-right">{moment(item.addedOn).format('DD MMM, YYYY')}</div>
-                      </div>
-                    }
-                    description={item.description}
-                  />
-              </List.Item>
-            )}
-          />
-        )
-      }
-      else {
-        return(
-          <p>No expenses recorded.</p>
-        )
-      }
+    let { expenseList } = this.props;
 
-      
+    if(expenseList.length) {
+      return(
+        <List
+          itemLayout="horizontal"
+          dataSource={expenseList}
+          renderItem={item => (
+            <List.Item
+              actions={[
+                'Edit', 
+                <Popconfirm
+                  title="Are you sure to delete this expense?"
+                  onConfirm={this.confirmDelete}
+                  okText="Yes"
+                  cancelText="No"
+                >
+                  <a href="#">Delete</a>
+                </Popconfirm>
+              ]}
+            >
+                <List.Item.Meta
+                  title={
+                    <div className="row">
+                        <div className="col-md-6">{item.amount}</div>
+                        <div className="col-md-6 text-right">{moment(item.addedOn).format('DD MMM, YYYY')}</div>
+                    </div>
+                  }
+                  description={item.description}
+                />
+            </List.Item>
+          )}
+        />
+      )
     }
+    else {
+      return(
+        <p>No expenses recorded.</p>
+      )
+    }
+
+    
+  }
 }
 
 export default ExpenseList;
